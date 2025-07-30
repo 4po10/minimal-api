7448a1da23279be393a44ebf2fe03d584add115a
Israel.Alexandre <israel4lexandre@gmail.com>
2025-07-29 22:53:46 -0300

    Criando rota Home respondendo por Json

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using MinimalApi.DTOs;
 using MinimalApi.Dominio.Interfaces;
 using MinimalApi.Dominio.Servicos;
 using Microsoft.AspNetCore.Mvc;
+using MinimalApi.Dominio.ModelViews;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,7 +26,8 @@ var app = builder.Build();
 
 
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.Json(new Home()));
+
 
 app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
